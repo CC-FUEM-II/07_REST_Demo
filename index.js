27da1245e2f2d502bb335c1fe74c02eb5d77af0f
hesse65 <152978535+hesse65@users.noreply.github.com>
2023-12-19 11:32:29 +0000

    Kleinen node . Server_Test

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+// Import
 
+const app = require('express')();
+const PORT = 8080;
+
+// Listener für express-instanz
+app.listen(
+       PORT,
+       () => console.log(`server ready at http://localhost"${PORT}`)
+   );
+
+
+/* Variante mit Code 200 */
+// app.get('/', (req, res) => {
+//     res.send(200);
+//   });
+
+/* Variante mit JSON */
+app.get("/", (req,res) =>
+{
+    res.status(200).send({
+        item: "T-shirt",
+        size: "L",
+        onstock: 20
+    })
+});
